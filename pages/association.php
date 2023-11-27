c38d706db15b3c12e6ee932deb39a8b9d7d62346
Didier 'OdyX' Raboud <odyx@raksha.ch>
2023-11-27 11:26:01 +0100

    Correction des contacts

--- pages/association.php
+++ pages/association.php
@@ -1,93 +1,76 @@
+<div id="main" class="eightcol">
+
+  <h1><span>Association des Amis de Charles-Albert Cingria</span></h1>
+
+  <p>
+    <strong>
+      <?php print $txts[0]['writing_content'];?>
+    </strong>
+    <!-- <img src="img/test.jpg" style="float:left; width: 120px; padding: 2% 2% 2% 0"> -->
+    <br /><br />
+
+  <div id="main_text">
+    <?php print $txts[1]['writing_content'];?>
+
+    <p>
+      <br />
+      Vers le site de l'éditeur :<br /><br />
+      <a href="https://www.lagedhomme.com/boutique/liste_rayons.cfm"
+        onclick="window.open(this.href, '_blank'); return false;"
+        class="pink_link_min_height">www.lagedhomme.com</a><br />
 
 
-       <div id="main" class="eightcol">
+    </p>
+  </div>
 
-        <h1><span>Association des Amis de Charles-Albert Cingria</span></h1>
+</div>
+<div id="intro" class="fourcol last">
+  <div id="yellow_box">
+    <span style="font-style: italic; margin-bottom: 12px; display: block;">Association des Amis de Charles-Albert
+      Cingria</span>
+    <strong>Date de création :</strong> 11 mai 1963<br />
+    <strong>Cotisation annuelle :</strong> CHF 30.-<br />
+    <br />
 
-                <p>
-        <strong><?php print $txts[0]['writing_content'];?></strong>
-        <!-- <img src="img/test.jpg" style="float:left; width: 120px; padding: 2% 2% 2% 0"> -->
-        <br/><br/>
+    <strong>Présidence :</strong>
+    <p>
+      Rudolf Mahrer<br />
+      Rue Centrale 8<br />
+      1804 Corsier-sur-Vevey<br />
+      <br />
+    </p>
 
-        <div id="main_text">
-        <?php print $txts[1]['writing_content'];?>
-
-        <p>
-           <br/>
-          Vers le site de l'éditeur :<br/><br/>
-            <a href="https://www.lagedhomme.com/boutique/liste_rayons.cfm" onclick="window.open(this.href, '_blank'); return false;"  class="pink_link_min_height">www.lagedhomme.com</a><br/>
-
-
-        </p>
-          </div>
-
-      </div>
-                       <div id="intro" class="fourcol last">
-                <div id="yellow_box">
-                  <span style="font-style: italic; margin-bottom: 12px; display: block;">Association des Amis de Charles-Albert Cingria</span>
-                  <strong>Date de création :</strong> 11 mai 1963<br/>
-                  <strong>Cotisation annuelle :</strong> CHF 30.-<br/>
-                  <strong>Nombre d'adhérents :</strong> 200 personnes<br/><br/>
-                  <strong>Présidente :</strong>
-
-                      <p>Maryke de Courten</br>
-                      Avenue Beaulieu 10</br>
-                      1180 Rolle</br>
-                      +41 21 825 24 94</br>
-                      <a href="mailto:antoine.decourten@bluewin.ch">Contact</a><br/><br/>
-                      </p>
-                      <strong>Vice-président :</strong>
-                      <p>Thierry Raboud</br>
-                      Chemin des Cornalles 2B</br>
-                      1802 Corseaux</br>
-</br>
-                      <a href="mailto:thierry@raboud.com">Contact</a><br/>
-                      <br/>
-                      </p>
-                      <strong>Trésorier :</strong>
-                      <p>Jean-Claude Morisod</br>
-                      Grand’Rue 12a</br>
-                      1700 Fribourg</br>
-                      +41 26 323 16 52</br>
-                      <br/>
-                      </p>
-                      <strong>Secrétaire :</strong>
-                      <p>Suzi Assal</br>
-                      Avenue des Belles-Roches 2</br>
-                      1004 Lausanne</br>
-                      +41 21 646 09 10</br>
-                      <a href="mailto:s.assal@bluewin.ch">Contact</a><br/>
-                      <br/>
-                      </p>
-                </div>
-               </div>
+    <strong>Vice-présidence :</strong>
+    <p>
+      Thierry Raboud<br />
+      Chemin des Cornalles 2B<br />
+      1802 Corseaux<br />
+      <br />
+    </p>
+    <strong>Trésorerie :</strong>
+    <p>
+      Alessio Christen<br />
+      Chemin de la Fenettaz 18<br />
+      1722 Bourguillon<br />
+      <br />
+    </p>
+  </div>
+</div>
 
 
-             <div class="fourcol"> 	
+<div class="fourcol">
 
+  <h2><span>Adhérer à l'Association</span></h2>
+  <br />
+  <div id="bulletin">
+    Devenez membre de l’Association et soyez régulièrement informé(e) de ses activités. Vous recevrez également les
+    Petites Feuilles chez vous.
+    <br /><br />
+    <a href="#" id="contact" class="pink_link_min_height">Lien de contact</a>
+    <span id="contact_info">
+      Thierry Raboud<br />
+      <a href="mailto:thierry@raboud.com" class="pink_link">thierry@raboud.com</a>
+    </span>
+  </div>
 
-          <h2><span>Adhérer à l'Association</span></h2>
-           <br/>
-        <div id="bulletin">
-        Devenez membre de l’Association et soyez régulièrement informé(e) de ses activités. Vous recevrez également les Petites Feuilles chez vous.
-        <br/><br/>
-        <a href="#" id="contact" class="pink_link_min_height">Lien de contact</a>
-        <span id="contact_info">
-          Thierry Raboud<br/>
-          <a href="mailto:thierry@raboud.com" class="pink_link">thierry@raboud.com</a>
-        </span>
-        <br/>
-
-
-        <!-- Téléchargez le bulletin d’adhésion –––––––––––––––––– ELEMENT SUPPRIMÉ –––––––––––––––––––––
-
-        <?php
-         foreach($documents as $row) {
-          $file_url  = $row['dir_name'].'/'.$row['upload_file_name'].$row['upload_file_ext'];	
-          print '<a href="http://www.cingria.ch/'.$file_url.'" class="pink_link_min_height">Télécharger le bulletin d\'adhésion</a>';
-            }
-         ?> --!>
-
-          </div>
-
-        </div>
+</div>

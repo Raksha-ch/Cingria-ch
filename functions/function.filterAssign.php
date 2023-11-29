23679cafb87bfd7a163979414ecc5023a14e8809
Didier 'OdyX' Raboud <odyx@raksha.ch>
2023-11-29 10:39:52 +0100

    Wget static dump, drop all PHP

--- functions/function.filterAssign.php
+++
@@ -1,7 +0,0 @@
-<?php
-function filterAssign($array, $debug = false) {
-  $filtered_array = array_filter($array); // filter empty values
-  foreach ($filtered_array as $k => $v) { $GLOBALS[$k] = $v; } // assign dynvar
-  if($debug == true) { print_r($filtered_array) ; }
-}
-?>
--- functions/function.menuItem.php
+++
@@ -1,7 +0,0 @@
-<?php
-function menu_item( $root, $id, $title, $current ) {
-  $state = ( $current == $id ) ? ' class="active" ' : '' ;
-    print '<a href="'.$root.$id.'" '.$state.'>'.$title.'</a>';
-};
-
-?>
